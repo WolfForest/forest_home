9a3784afb015f5f0f89a742b53e7d16d3c32a7fd
Wolf Forest <test@wolfforest.ru>
2017-11-28 20:52:09 +0500

    test response api

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,35 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({
+    status: 'testing',
+    time: Date.now(),
+    groups: [
+      {
+        id: 'room_1',
+        title: 'Room 1'
+      }
+    ],
+    tumblers: [
+      {
+        id: 101,
+        type: 'light',
+        status: 'working',
+        enabled: true,
+        title: 'light 1',
+        group: 'room_1'
+      }
+    ],
+    sensors: [
+      {
+        id: 201,
+        status: 'working',
+        value: Math.random()*10,
+        title: 'noise',
+        group: 'room_1'
+      }
+    ]
+  });
 });
 
 module.exports = router;
